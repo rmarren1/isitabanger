193f18962b9a56e669659f0585268fc3492735b0
Ryan Marren <rymarr@tuta.io>
2018-11-24 11:17:46 -0500

    Re organize analysis and create model.

---
+++ model/pull_data.py
@@ -0,0 +1,21 @@
+import json
+import pandas as pd
+from api import api
+from pymongo import MongoClient
+
+with open('../secrets/dbconn.json', 'r') as f:
+    conn = json.load(f)
+    db = MongoClient(
+        '{}:{}'.format(conn['url'], conn['port']),
+        username=conn['username'],
+        password=conn['password'],
+        authSource=conn['database']
+    ).banger
+
+data = list(db.votes.find())
+D = pd.DataFrame(data)
+ids = D['songId'].tolist()
+responses = [api.get(endpoint='audio-features', id=i) for i in ids]
+R = pd.DataFrame([r.json() for r in responses])
+S = R.merge(D, on='id', how='outer')
+S.to_csv('data.csv', index=False)
